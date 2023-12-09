ab93a9c5af65f0611f90fb1e1ccf7582fbe0472d
Mariia Pisniia <112613352+mariiapi@users.noreply.github.com>
2023-12-09 23:06:53 +0200

    Add files via upload

---
+++ Task2_5.java
@@ -0,0 +1,44 @@
+package hw_6;
+
+import java.util.Arrays;
+
+public class Task2_5 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        int[][] transposedMatrix = transposeMatrix(matrix);
+
+        System.out.println("\nTransposed Matrix:");
+        printMatrix(transposedMatrix);
+    }
+
+    // Функція для транспонування матриці
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transposed = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
